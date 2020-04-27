dc21d27187b6f975a4017048bad099d3e10db6b9
Bruno Sprotte <brunosprotte@hotmail.com>
2020-04-26 23:02:57 -0300

    adiciona dockerfile e log de inicialização do servidor

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
 const app = require('./app');
 
-app.listen(process.env.PORT || 3333);
+app.listen(process.env.PORT || 3333, () => {
+  console.log('Be The Hero server is running...');
+  console.log('listening on 3333');
+});
